135c1cf42401db0d96c1cbac9b40d52cff33df17
Constance <constancellc@gmail.com>
2017-07-10 16:56:42 +0100

    no message

--- powerflow.py
+++ powerflow.py
@@ -1,183 +1,159 @@
 from cvxopt import matrix, spdiag, solvers, sparse
 import numpy as np
 
-#nodes = [650,646,645,632,633,634,611,684,671,692,975,652,680]
-nodes = {650:0,646:0,645:2,632:3,633:4,634:5,611:6,684:7,671:8,692:9,675:10,
-         652:11,680:12}
+from test4 import nodes, lines, spotLoads, Z, transformers, slackbus
 
-# Node A, Node B, Length (ft), Config.
-lines = [[632,645,500,'603'],
-         [632,633,500,'602'],
-         [633,634,0,'XFM-1'],
-         [645,646,300,'603'],
-         [650,632,2000,'601'],
-         [684,652,800,'607'],
-         [632,671,2000,'601'],
-         [671,684,300,'604'],
-         [671,680,1000,'601'],
-         [671,692,0,'Switch'],
-         [684,611,300,'605'],
-         [692,675,500,'606']]
-
-# Node, Phase 1 (kW), Phase 1 (kVar), Phase 2 (kW), Phase 2 (kVar),
-# Phase 3 (kW), Phase 3 (kVar)
-spotLoads = {634:[160000,110000,120000,90000,120000,90000],
-             645:[0,0,170000,125000,0,0],
-             646:[0,0,230000,132000,0,0],
-             652:[128000,86000,0,0,0,0],
-             671:[385000+9000,220000+5000,385000+33000,220000+19000,
-                  385000+59000,220000+34000],
-             675:[485000,190000-200000,68000,60000-200000,290000,
-                  212000-200000],
-             692:[0,0,0,0,170000,151000],
-             611:[0,0,0,0,170000,80000-100000],
-             632:[9000,5000,33000,19000,59000,34000]}
-
-# Node A, Node B, Phase 1 (kW), Phase 1 (kVar), Phase 2 (kW), Phase 2 (kVar),
-# Phase 3 (kW), Phase 3 (kVar)
-#distLoads = [[632,671,17,10,66,38,117,68]]
-
-# in Ohnms per mile
-Z = {'601':[[complex(0.3465,1.0179),complex(0.1560,0.5017),
-             complex(0.1580,0.4236)],
-            [complex(0.3375,1.0478),complex(0.1535,0.3849)],
-            [complex(0.3414,1.0348)]],
-     '602':[[complex(0.7526,1.1814),complex(0.1580,0.4236),
-             complex(0.1560,0.5017)],
-            [complex(0.7475,1.1983),complex(0.1535,0.3849)],
-            [complex(0.7436,1.2112)]],
-     '603':[[complex(0.0000,0.0000),complex(0.0000,0.0000),
-             complex(0.0000,0.0000)],
-            [complex(1.3294,1.3471),complex(0.2066,0.4591)],
-            [complex(1.3238,1.3569)]],
-     '604':[[complex(1.3238,1.3569),complex(0.0000,0.0000),
-             complex(0.2066,0.4591)],
-            [complex(0.0000,0.0000),complex(0.0000,0.0000)],
-            [complex(1.3294,1.3471)]],
-     '605':[[complex(0.0000,0.0000),complex(0.0000,0.0000),
-             complex(0.0000,0.0000)],
-            [complex(0.0000,0.0000),complex(0.0000,0.0000)],
-            [complex(1.3292,1.3475)]],
-     '606':[[complex(0.7982,0.4463),complex(0.3192,0.0328),
-             complex(0.2849,-0.0143)],
-            [complex(0.7891,0.4041),complex(0.3192,0.0328)],
-            [complex(0.7982,0.4463)]],
-     '607':[[complex(1.3425,0.5124),complex(0.0000,0.0000),
-             complex(0.0000,0.0000)],
-            [complex(0.0000,0.0000),complex(0.0000,0.0000)],
-            [complex(0.0000,0.0000)]],
-     'Switch':[[complex(0.000,0.000),complex(0.0000,0.0000),
-             complex(0.0000,0.0000)],
-            [complex(0.0000,0.0000),complex(0.0000,0.0000)],
-            [complex(0.0000,0.0000)]]}
-          
 N = len(nodes) # number of nodes
 L = len(lines) # number of lines
 
+lineVariables = []
+for i in range(0,len(lines)):
+    line = lines[i]
+    if i == 0:
+        nPhases = len(line[4])
+    for ph in range(0,nPhases):
+        if line[4][ph] == 0:
+            continue
+        lineVariables.append([i,ph,'p'])
+        lineVariables.append([i,ph,'q'])
+
+nodeVariables = []
+slackVariables = []
+i = 0
+for node in nodes:
+    for ph in range(0,nPhases):
+        if nodes[node][ph] == 0:
+            continue
+        nodeVariables.append([i,node,ph])
+        if node == slackbus[0]:
+            slackVariables.append(i)
+        i += 1
+print nodeVariables
+print slackVariables
 R = []
 X = []
 
-for i in range(0,L):
-    length = lines[i][2] # ft
-    length = length*0.00018939 # ft -> miles
-    config = lines[i][3]
-
-    for ph in range(0,3):
-        try:
-            R.append(length*Z[config][ph][0].real)
-            X.append(length*Z[config][ph][0].imag)
-        except:
-            R.append(0.0)
-            X.append(0.0)
-        
-
-# Acutally first I want to construct a Y matrix
-'''
-Y = matrix(complex(0.0,0.0),(N,N))
-
-for i in range(0,L):
-    line = lines(i)
-    nodeA = line[0]
-    nodeB = line[1]
-    length = line[2]
+for line in lines:
+    length = float(line[2])*0.00018939 # ft -> miles
     config = line[3]
 
-    zPerMile = Z[config]
+    r = []
+    x = []
 
-    yab = complex(0.0,0.0)
-'''
+    if config in transformers:
+        for ph in range(0,nPhases):
+            if line[4][ph] == 0:
+                r.append(0.0)
+                x.append(0.0)
+            else:
+                r.append(transformers[config][0])
+                x.append(transformers[config][1])
 
-# changed my mind, just going straight in with the constraints\
-# first considering all of the Pij terms
+    else:
+        for ph in range(0,nPhases):
+            r.append(length*Z[config][ph][0].real)
+            x.append(length*Z[config][ph][0].imag)
 
-# I need to determine my decision variables - ie. which Pij exist
-# dumb version - assume 3L Pijs and 3L Qijs and 3N Vis
-# order = Pij1 Pij2 Pij3 Qij1 Qij2 Qij3 Pjk1 Pjk2 .... Vi1 Vi2 Vi3 Vj1 ... VN3
-#X = [0.0]*(6*L)
+    R.append(r)
+    X.append(x)
+    
+M = len(nodeVariables)+len(lineVariables)
 
-# starting the matrix of constraints
-A = matrix(0.0,(6*L+3*N,6*L+3*N))
-b = matrix(0.0,(6*L+3*N,1))
+A = matrix(0.0,(M,M))
+b = matrix(0.0,(M,1))
 
+for variable in lineVariables:
+    line = lines[variable[0]]
+    ph = variable[1]
+    kind = variable[2]
 
-# slack bus
-for ph in range(0,3):
-    A[9*L+ph,6*L+3*nodes[650]+ph] = 1.0
-    b[9*L+ph] = 122 # V
-
-
-for l in range(0,L):
-    line = lines[l]
     nodei = line[0]
     nodej = line[1]
 
-    Vi_index = nodes[nodei]
-    Vj_index = nodes[nodej]
+    A[variable[0],variable[0]] = 1.0 # line flow in question
 
-    try:
-        loads = spotLoads[nodej]
-    except:
-        loads = [0,0,0,0,0,0]
+    # now look for things connected to j
+    for variable2 in lineVariables:
+        if lines[variable2[0]][1] != nodej:
+            continue
 
-    for ph in range(0,3):
-        A[6*l+ph,6*l+ph] = 1.0 # p I think this bit is wrong
-        A[6*l+ph+3,6*l+ph+3] = 1.0 # q
+        if variable[1] != variable2[1]:
+            continue
 
-        A[6*L+3*l+ph,6*L+3*Vi_index+ph] = 1.0 # Vi
-        A[6*L+3*l+ph,6*L+3*Vj_index+ph] = -1.0 # Vj
-        A[6*L+3*l+ph,6*l+ph] = -R[l] # Pij
-        A[6*L+3*l+ph,6*l+ph+3] = -X[l] # Qij
+        if variable[2] != variable2[2]:
+            continue
+
+        # so variable 2 leaves from j and is the same phase and type as variable
+        A[variable2[0],variable2[0]] = -1.0
+
+    # lastly look for real loads at j
+
+    if kind == 'p':
+        offset = 0
+    elif kind == 'q':
+        offset = 1
         
-        # look for other things connected to j
-        for l2 in range(0,L):
-            if lines[l2][0] != nodej:
-                continue
+    try:
+        load = float(spotLoads[nodej][ph*2+offset])
+    except:
+        load = 0.0
 
-            # so we now know that line ii connects j to something
-            A[6*l+ph,6*l2+ph] = -1.0
-            A[6*l+ph+3,6*l2+ph+3] = -1.0
+    b[variable[0]] = load
 
-        b[6*l+ph] = float(loads[2*ph])
-        b[6*l+ph+3] = float(loads[2*ph+1])
+    r = R[variable[0]][ph]
+    x = R[variable[0]][ph]
 
-print A
-X = np.linalg.solve(A,b)
+    for node in nodeVariables:
+        if node[2] != ph:
+            continue
 
+        if node[1] == nodei:
+            Vi_index = node[0]
+        elif node[1] == nodej:
+            Vj_index = node[0]
+
+    print Vj_index
+
+    if kind == 'p':           
+        A[len(lineVariables)+Vj_index,len(lineVariables)+Vi_index] = 1.0 # Vi   
+        A[len(lineVariables)+Vj_index,len(lineVariables)+Vj_index] = 1.0 # Vj
+        
+        A[len(lineVariables)+Vj_index,variable[0]] = r # Pij  
+
+    else:
+        A[len(lineVariables)+Vj_index,variable[0]] = x # Qij
+     
+# and the slack bus
+for V_index in slackVariables:
+    A[len(lineVariables)+V_index,len(lineVariables)+V_index] = 1.0
+    b[V_index] = float(slackbus[1])
+    print V_index
+
+   
+sol = np.linalg.solve(A,b)
+print sol
+'''
 linesPower = []
+nodeVoltages = []
 
 c = 0
 for i in range(0,L):
     line = []
-    for ii in range(0,6):
-        line.append(X[c][0])
+    for ii in range(0,2*nPhases):
+        line.append(sol[c][0])
         c += 1
     linesPower.append(line)
+for i in range(L,L+N):
+    node = []
+    for ii in range(0,nPhases):
+        node.append(sol[c][0])
+        c += 1
+    nodeVoltages.append(node)
 
 linesPower = matrix(linesPower)
 print linesPower.T
-    
-    
-    
-
+nodeVoltages = matrix(nodeVoltages)
+print nodeVoltages.T
+'''
 
 
